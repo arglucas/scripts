fadba21f358f29b29306d4adbcdf50a824e80e4c
Alex Lucas <arglucas@hotmail.com>
2016-07-20 14:11:09 +0000

    Added new script for finding an AMI id for all regions and a comment clarification on create-instance-al-ssh.sh

--- create-instance-al-ssh.sh
+++ create-instance-al-ssh.sh
@@ -4,7 +4,7 @@
 
 # AL2015.09 eu-west-1
 IMAGE=ami-69b9941e
-# SSH SG
+# SSH SG (replace with target)
 SG=sg-522a2330
 
 ID=$(aws ec2 run-instances --image-id $IMAGE --instance-type t2.medium --key-name DevKey --associate-public-ip-address --security-group-ids $SG | jq -r '.Instances[].InstanceId')
